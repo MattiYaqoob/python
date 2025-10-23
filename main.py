17f69978b3521e3ec900301c1e7e39a340c24010
MattiYaqoob <matishahad2009@yahoo.com>
2025-10-23 12:48:55 +0200

    my pirst python

---
+++ main.py
@@ -0,0 +1,53 @@
+# This tag for comments 
+# print code 
+#print("Hello, World!")
+# End of the code
+# This is a simple Python script that prints "Hello, World!" to the console.
+
+# Variables and Data Types in Python
+#x = 5
+#y = 10
+#sum = x + y
+#print("This sum of x = ",x, " and the y = ",y, "this is the sum =",sum)
+
+# Variable naming conventions in Python
+#myvar = "John"
+#my_var = "John"
+#_my_var = "John"
+#myVar = "John"
+#MYVAR = "John"
+#myvar2 = "John"
+
+
+# Multiple variable assignment
+#x, y, z = "Orange", "Banana", "Cherry"
+#print(x)
+#print(y)
+#print(z)
+
+# One value to multiple variables
+#x = y = z = "Orange"
+#print(x)
+#print(y)
+#print(z)
+
+
+# Unpack a collection
+#fruits = ["apple", "banana", "cherry"]
+#x, y, z = fruits
+#print(x)
+#print(y)
+#print(z)
+
+
+# Output variables
+
+#x = 22
+
+#def myfunc():
+ #   y = 300
+  #  print(x + y)
+
+#myfunc()
+
+
